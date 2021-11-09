9b62651477de1814fc5e6b24e9ec27308fb4e5f8
juandsoto <juandsoto1006@gmail.com>
2021-11-09 15:10:39 -0500

    Lazyload basico listo

--- src/01-lazyload/pages/LazyPage1.tsx
+++ src/01-lazyload/pages/LazyPage1.tsx
@@ -1,3 +1,4 @@
 export const LazyPage1 = () => {
   return <h1>LazyPage1</h1>;
 };
+export default LazyPage1;
--- src/routes/Navigation.tsx
+++ src/routes/Navigation.tsx
@@ -1,47 +1,45 @@
-import { BrowserRouter as Router, Switch, Route, NavLink } from 'react-router-dom';
+import { Suspense } from 'react';
+import { BrowserRouter as Router, Switch, Route, NavLink, Redirect } from 'react-router-dom';
 
 import logo from '../logo.svg';
-import { LazyPage1, LazyPage2, LazyPage3 } from '../01-lazyload/pages';
+
+import { routes } from './routes';
 
 export const Navigation = () => {
   return (
-    <Router>
-      <div className='main-layout'>
-        <nav>
-          <img src={logo} alt='React Logo' />
-          <ul>
-            <li>
-              <NavLink to='/lazy1' activeClassName='nav-active' exact>
-                Lazy1
-              </NavLink>
-            </li>
-            <li>
-              <NavLink to='/lazy2' activeClassName='nav-active' exact>
-                Lazy2
-              </NavLink>
-            </li>
-            <li>
-              <NavLink to='/lazy3' activeClassName='nav-active' exact>
-                Lazy3
-              </NavLink>
-            </li>
-          </ul>
-        </nav>
+    //que mostrar mientras se cargan los componentes ? Suspense
+    <Suspense fallback={<span>Loading...</span>}>
+      <Router>
+        <div className='main-layout'>
+          <nav>
+            <img src={logo} alt='React Logo' />
+            <ul>
+              {routes.map(({ path, name }) => (
+                <li key={name}>
+                  <NavLink to={path} activeClassName='nav-active' exact>
+                    {name}
+                  </NavLink>
+                </li>
+              ))}
+            </ul>
+          </nav>
 
-        {/* A <Switch> looks through its children <Route>s and
+          {/* A <Switch> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
-        <Switch>
-          <Route path='/lazy1'>
-            <LazyPage1 />
-          </Route>
-          <Route path='/lazy2'>
-            <LazyPage2 />
-          </Route>
-          <Route path='/lazy3'>
-            <LazyPage3 />
-          </Route>
-        </Switch>
-      </div>
-    </Router>
+          <Switch>
+            {routes.map(({ name, path, component: Component }) => (
+              <Route
+                key={name}
+                path={path}
+                render={() => {
+                  return <Component />;
+                }}
+              />
+            ))}
+          </Switch>
+          <Redirect to={routes[0].path} />
+        </div>
+      </Router>
+    </Suspense>
   );
 };
